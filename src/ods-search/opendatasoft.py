2b027700769bc721ead5b36bd23ac357d9eef674
raiyaj <raiyajessa@gmail.com>
2021-07-03 21:20:36 -0700

    feat(ods): add initial ods class

---
+++ src/ods-search/opendatasoft.py
@@ -0,0 +1,39 @@
+import requests
+
+
+class Opendatasoft:
+  """
+  Core Opendatasoft object; stores session info and default API settings.
+  """
+  def __init__(
+    self,
+    data_portal_id: str = 'data',
+    base_url: str = None,
+    api_key: str = None,
+    session: requests.Session = None,
+    timezone: str = 'UTC'
+  ) -> None:
+    """
+    :param data_portal_id: Id used to create the base API URL, eg.
+      {data_portal_id}.opendatasoft.com. Default: `data`
+    :param base_url: Custom domain to override the base API URL created by
+      `data_portal_id`
+    :param api_key: API key for restricted datasets
+    :param session: A session object with which to make API calls
+    :param timezone: Default timezone for datetime fields in queries and
+      responses. Default: `UTC`
+    """
+    if base_url:
+      self.base_url = base_url.strip('/')
+      self.data_portal_id = None
+    else:
+      self.base_url = f'{data_portal_id}.opendatasoft.com'
+      self.data_portal_id = data_portal_id
+    self.base_url += '/api/v2'
+  
+    self.api_key = api_key
+    self.session = session or requests.Session()
+    # if self.api_key:
+    #   self.session.auth = 
+
+    self.timezone = timezone
