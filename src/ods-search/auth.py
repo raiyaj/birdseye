3051dc1ad4019098d3c74ffa813feb4378eb7d80
raiyaj <raiyajessa@gmail.com>
2021-07-05 19:16:53 -0700

    feat(ods): add source parameter

--- src/ods-search/auth.py
+++ src/ods-search/auth.py
@@ -4,9 +4,9 @@ import requests
 class TokenAuth(requests.AuthBase):
   """API key authentication"""
 
-  def __init__(self, api_key: str):
+  def __init__(self, api_key: str) -> None:
     self.api_key = api_key
   
-  def __call__(self, request: requests.Request):
+  def __call__(self, request: requests.Request) -> requests.Request:
     request.headers['Authorization'] = f'Apikey {self.api_key}'
     return request
